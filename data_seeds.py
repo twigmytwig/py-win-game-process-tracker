21e965f00e5c95109f73e716a685c7ed653daf8b
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2024-05-05 08:46:52 -0400

    ADD: auto adding entries to data.json

--- data_seeds.py
+++ data_seeds.py
@@ -7,6 +7,8 @@ data : str = []
 def test_data():
     tempData = "Fallout76.exe"
     tempData2 = "RocketLeague.exe"
+    tempData3 = "FortniteClient-Win64-Shipping.exe"
     data.append(tempData2)
     data.append(tempData)
+    data.append(tempData3)
     return data
--- save_to_file.py
+++ save_to_file.py
@@ -24,8 +24,16 @@ def save_to_file(closed_game_data, closed_game_name,filename):
                 game_time_data[x]["total_game_time_seconds"] = total_time_seconds
                 with open(filename, 'w') as data_file:
                     json.dump(game_time_data, data_file, indent=2)
+                return
+        #If you got here that means the file isn't empty but it also doesn't have an entry
+        previous_game_time = datetime.strptime(closed_game_data[closed_game_name],format)
+        new_entry_seconds = float(get_game_time_seconds(previous_game_time))
+        new_entry = { closed_game_name: { "total_game_time_seconds": new_entry_seconds}}
+        game_time_data.update(new_entry)
+        with open(filename, 'w') as data_file:
+                    json.dump(game_time_data, data_file, indent=2)
 
     else:
-        print("in else")
+        #tis means the file is empty. TODO: this isn't initializing a proper file
         with open(filename, 'w') as data_file:
             json.dump(closed_game_data, data_file)
